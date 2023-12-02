b0be9c23476c5ab44d7fc226c363867319ad2106
taycannon <taycannon2003@gmail.com>
2023-12-01 18:43:57 -0600

    Added/updated the app.jsx with correct links and code

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,33 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
-
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import SignupForm from './components/SignupForm';
+import LoginForm from './components/LoginForm';
+
+// Created the Apollo Client
+const client = new ApolloClient({
+  uri: '/graphql', 
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/login" element={<LoginForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
